769e15b5d1457bbf2bb8d8d990abfe6c465f49bb
Ahmed Basioni <eng.ahmed.bassioni@gmail.com>
2016-03-21 05:22:09 +0200

    C#20
    Contacts Bug fixing

--- modules/contacts/controllers/contactsController.js
+++ modules/contacts/controllers/contactsController.js
@@ -1,5 +1,5 @@
 MenuApp.controller('contactsController', function($scope , $http) {
-	$http.get("http://www.egyptbooking.org/SMS_Notifications_Manager/modules/contacts/Search.php)
+	$http.get("http://www.egyptbooking.org/SMS_Notifications_Manager/modules/contacts/Search.php")
     .then(function (response) {
 	$scope.contactslist = response.data.contacts;
 	});
